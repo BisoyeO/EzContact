f66f624c5e02bd5b0870170a1bb8919fae15938f
Bisoye Olaleye <iSoy@Bisoyes-MacBook-Pro.local>
2015-04-12 23:12:54 -0500

    Clean up

--- app/src/main/java/com/isoy/ezcontact/GenerateCodeActivity.java
+++ app/src/main/java/com/isoy/ezcontact/GenerateCodeActivity.java
@@ -10,10 +10,8 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.WriterException;
